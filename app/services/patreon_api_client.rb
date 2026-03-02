25f8b3a9726a0887fdb951fa7aacb9d6f7493bef
theCrius <claudio.vallesi@proton.me>
2026-03-02 19:09:51 +0000

    Implement v1-specific API endpoints (use /current_user/campaigns and /pledges)

--- app/services/patreon_api_client.rb
+++ app/services/patreon_api_client.rb
@@ -19,11 +19,27 @@ module DiscoursePatreonDonations
     end
 
     def fetch_campaign_data
-      endpoint = '/campaigns'
-      params = { 'fields[campaign]' => 'patron_count,is_monthly,creation_name' }
-      
-      response = make_request(endpoint, params)
-      response&.dig('data', 0)
+      if @api_version == 'v1'
+        fetch_campaign_data_v1
+      else
+        fetch_campaign_data_v2
+      end
+    end
+
+    def fetch_all_campaigns
+      if @api_version == 'v1'
+        fetch_all_campaigns_v1
+      else
+        fetch_all_campaigns_v2
+      end
+    end
+
+    def fetch_members
+      if @api_version == 'v1'
+        fetch_members_v1
+      else
+        fetch_members_v2
+      end
     end
 
     def discover_campaign_id(campaign_url)
@@ -40,7 +56,18 @@ module DiscoursePatreonDonations
       matching_campaign&.dig('id')
     end
 
-    def fetch_all_campaigns
+    private
+
+    # V2 API methods
+    def fetch_campaign_data_v2
+      endpoint = '/campaigns'
+      params = { 'fields[campaign]' => 'patron_count,is_monthly,creation_name' }
+      
+      response = make_request(endpoint, params)
+      response&.dig('data', 0)
+    end
+
+    def fetch_all_campaigns_v2
       endpoint = '/campaigns'
       params = { 'fields[campaign]' => 'vanity,patron_count,is_monthly,creation_name' }
       
@@ -48,7 +75,7 @@ module DiscoursePatreonDonations
       response&.dig('data') || []
     end
 
-    def fetch_members
+    def fetch_members_v2
       all_members = []
       cursor = nil
 
@@ -71,7 +98,47 @@ module DiscoursePatreonDonations
       all_members
     end
 
-    private
+    # V1 API methods
+    def fetch_campaign_data_v1
+      endpoint = '/current_user/campaigns'
+      params = {}
+      
+      response = make_request(endpoint, params)
+      # V1 returns campaigns in 'data' array
+      campaigns = response&.dig('data') || []
+      campaigns.first
+    end
+
+    def fetch_all_campaigns_v1
+      endpoint = '/current_user/campaigns'
+      params = {}
+      
+      response = make_request(endpoint, params)
+      response&.dig('data') || []
+    end
+
+    def fetch_members_v1
+      all_members = []
+      cursor = nil
+
+      loop do
+        # V1 uses 'pledges' endpoint instead of 'members'
+        endpoint = "/campaigns/#{@campaign_id}/pledges"
+        params = {
+          'page[count]' => '1000'
+        }
+        params['page[cursor]'] = cursor if cursor
+
+        response = make_request(endpoint, params)
+        break unless response
+
+        all_members.concat(response['data'] || [])
+        cursor = response.dig('meta', 'pagination', 'cursors', 'next')
+        break unless cursor
+      end
+
+      all_members
+    end
 
     def make_request(endpoint, params = {})
       uri = build_uri(endpoint, params)
